47258e3854968ed6919fdeea140286f14f3c79ef
icyflame <kannan.siddharth12@gmail.com>
2014-07-07 16:07:30 +0530

    Moved all the code for creating a new table to a different file.
    Now, in case the definition of the table changes (new columns are
    added or removed) then the table can be displayed appropriately by simply
    changing this one file, instead of having to change code at all the places
    that the table is displayed.

---
+++ homepage.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+
+require 'checklogin.php';
+require 'logoutbtn.php';
+require 'connect.php';
+
+$user = $_SESSION['username'];
+
+echo "<b>Tasks assigned to you</b><br/><br/>";
+
+// display all the tasks assigned to user.
+
+$query = "SELECT * FROM `tasks` WHERE assignee='$user'";
+
+$res = mysql_query($query);
+
+if(mysql_num_rows($res) == 0){
+    echo "No tasks have been assigned to you.<br/><br/>";
+}
+
+else{
+
+	include('disp-res-table.php');
+
+}
+
+echo "<b>Tasks assigned by you</b><br/><br/>";
+
+$query = "SELECT * FROM `tasks` WHERE assignor='$user'";
+
+$res = mysql_query($query);
+
+if(mysql_num_rows($res) == 0){
+	echo "No tasks have been assigned by you.<br/><br/>";
+}
+
+else{
+
+	include('disp-res-table.php');
+
+}
+
+?>
+
+<a href="assign-work.php">Assign a new task</a> <br/>
+<a href="display-table.php">View all ongoing tasks</a> <br/>
+<a href="userslist.php">Look at a list of all registered users</a> <br/>
