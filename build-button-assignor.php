d3ca969508222b8bcf4d8665cab1dea72ee151b9
icyflame <kannan.siddharth12@gmail.com>
2014-07-11 16:30:03 +0530

    Completed task completion for both assignor and assignee.
    The assignee can apply for completion, after that, the assignor may or
    may not accept that. He can either revert it ongoing or mark the task as completed.
    
    [issue] On the display table view the status can be changed. This needs to be fixed.

--- build-button-assignor.php
+++ build-button-assignor.php
@@ -4,7 +4,7 @@ if($stat == 0){
 
 	echo "Task ongoing.";
 
-	echo "<form action='updatestat.php' method='POST'><input style='display: none;' name='$tid' value='1'><input type='submit' value='Apply for Completion'></form>";
+	echo "<form action='updatestat.php' method='POST'><input style='display: none;' name='$tid' value='2'><input type='submit' value='Mark as completed'></form>";
 
 }
 
@@ -13,6 +13,7 @@ elseif($stat == 1){
 	echo "Applied for completion.";
 
 	echo "<form action='updatestat.php' method='POST'><input style='display: none;' name='$tid' value='0'><input type='submit' value='Revert to ongoing'></form>";
+	echo "<form action='updatestat.php' method='POST'><input style='display: none;' name='$tid' value='2'><input type='submit' value='Mark as completed'></form>";
 
 }
 
@@ -21,6 +22,5 @@ elseif($stat == 2){
 	echo "Task has been completed.";
 
 }
-
 
 ?>
--- disp-res-table-noform.php
+++ disp-res-table-noform.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 echo "<table border=2 cellpadding=10><tr><th>Deadline</th><th>Description</th><th>Assignor</th><th>Assignee</th><th>Date Added</th><th>Remarks</th><th>Status</th></tr>";
 
 while($row = mysql_fetch_assoc($res)){
@@ -13,6 +13,39 @@ while($row = mysql_fetch_assoc($res)){
 	$stat = $row['taskstatus'];
 
 	echo "<tr><td>$deadline</td><td>$desc<td>$nor</td><td>$nee</td><td>$adddate</td><td>$rem</td><td>$stat</td></tr>";
+
+}
+
+echo "</table>";
+*/
+?>
+
+<?php
+
+echo "<table border=2 cellpadding=10><tr><th>Deadline</th><th>Description</th><th>Assignor</th><th>Assignee</th><th>Date Added</th><th>Remarks</th><th>Status</th></tr>";
+
+while($row = mysql_fetch_assoc($res)){
+
+	$tid = $row['taskid'];
+	$desc = $row['taskdesc'];
+	$nor = $row['assignor'];
+	$nee = $row['assignee'];
+	$deadline = $row['deadline'];
+	$adddate = $row['adddate'];
+	$rem = $row['remarks'];
+	$stat = $row['taskstatus'];
+
+	echo "<tr>";
+	echo "<td>$deadline</td>";
+	echo "<td>$desc<td>$nor</td>";
+	echo "<td>$nee</td>";
+	echo "<td>$adddate</td>";
+	echo "<td>$rem</td>";
+	echo "<td>";
+
+	include('build-button-assignor.php');
+
+	echo "</td></tr>";
 
 }
 
